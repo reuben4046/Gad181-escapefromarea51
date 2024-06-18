11da7a8f42f022af84a71e3e24c98c85a906f4ca
reuben4046 <poolereuben@gmail.com>
2024-06-18 16:19:05 +1000

    update

--- Assets/Reuben/Scripts/PlayerMovementRP.cs
+++ Assets/Reuben/Scripts/PlayerMovementRP.cs
@@ -13,16 +13,14 @@ public class PlayerMovementRP : MonoBehaviour
 
     private float maxSpeed = 20f;
 
-    private float rotationSpeed = 0.2f;
+    private float rotationSpeed = 0.3f;
 
-    [SerializeField] private float moveSpeed = 20f;
+    [SerializeField] private float moveSpeed = 25f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        xAxis = Input.GetAxis("Horizontal");
-        yAxis = Input.GetAxis("Vertical");
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -43,38 +41,21 @@ public class PlayerMovementRP : MonoBehaviour
         {
             RotateRight();
         }
-        //if (rigidBody.velocity.magnitude > maxSpeed)
-        //{
-        //    rigidBody.velocity = Vector2.ClampMagnitude(rigidBody.velocity, maxSpeed);
-        //}
     }
 
-
-    //private void FixedUpdate()
-    //{
-        //ForwardForce();
-      //  if (Rigidbody2D.velocity.magnitude > maxSpeed)
-      //  {
-      //      Rigidbody2D.velocity = Rigidbody2D.velocity.normalized * maxSpeed;
-      //  }
-   // }
 
     private void RotateLeft()
     {
-        transform.Rotate(0, 0, rotationSpeed);
+        transform.Rotate(0, 0, - rotationSpeed);
     }
     private void RotateRight()
     {
-        transform.Rotate(0,0, -rotationSpeed);
+        transform.Rotate(0,0, rotationSpeed);
     }
    
 
     void ForwardForce()
     {
         transform.Translate(new Vector2(0, moveSpeed)* Time.deltaTime);
-        //rigidBody.velocity = transform.up * moveSpeed;
-
-        //transform.Translate(transform.up * Time.deltaTime, Space.Self); 
-        //rigidBody.AddForce(transform.up * forwardForce);
     }
 }
