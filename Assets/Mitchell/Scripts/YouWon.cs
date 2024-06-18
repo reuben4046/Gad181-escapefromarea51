a6b705a4150d57c15aa5209121f48d17077fdb18
SAE-1032443 <1032443@student.sae.edu.au>
2024-06-18 14:21:49 +1000

    Added YouWon Script from my Game "Pill"

---
+++ Assets/Mitchell/Scripts/YouWon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWon : MonoBehaviour
+{
+    private int docPickup;
+
+    // Collition code if the Player collects all 5 Coins to win.
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Doc")
+        {
+            docPickup++;
+            Debug.Log(docPickup);
+            Debug.Log("Doc Picked Up!");
+            if (docPickup >= 5)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+
+}
