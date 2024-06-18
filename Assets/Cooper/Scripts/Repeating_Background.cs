d08ea08628421cfa007d071aaa3c655b4f29ffcc
Cooper-Rampling <134895307+Cooper-Rampling@users.noreply.github.com>
2024-06-18 17:21:13 +1000

    Added tillable road

--- Assets/Cooper/Scripts/Repeating_Background.cs
+++ Assets/Cooper/Scripts/Repeating_Background.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ground : MonoBehaviour
+public class Repeating_Background : MonoBehaviour
 {
+    Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = new Vector3(5,0,-80);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z > startPos.z + 90) 
+        { 
+            transform.position = startPos;
+        }
     }
 }
