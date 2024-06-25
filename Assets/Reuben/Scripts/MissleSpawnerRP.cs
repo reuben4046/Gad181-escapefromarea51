1ce01fce1e08a98324b500120bf4ea58c1fa5725
reuben4046 <poolereuben@gmail.com>
2024-06-25 21:46:09 +1000

    fixed the spawn so that it isnt just in the corners

--- Assets/Reuben/Scripts/MissleSpawnerRP.cs
+++ Assets/Reuben/Scripts/MissleSpawnerRP.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class MissleSpawnerRP : MonoBehaviour
 {
+    [Header("Gizmos")]
+    [SerializeField] private float gizmoMaxXRange = 50;
+    [SerializeField] private float gizmoMaxYRange = 40;
 
-    [SerializeField] private float maxXRange = 50;
-    [SerializeField] private float maxYRange = 40;
+    [SerializeField] private float gizmoMinXRange = 35;
+    [SerializeField] private float gizmoMinYRange = 25;
 
-    [SerializeField] private float minXRange = 35;
-    [SerializeField] private float minYRange = 25;
+    [SerializeField] private float gizPositionX = 0f;
+    [SerializeField] private float gizPositionY = 0f;
 
+
+    [Header("Spawn")]
+    [SerializeField] private float xRangeMax = 25f;
+    [SerializeField] private float xRangeMin = -25f;
+
+    [SerializeField] private float yRangeMax = 20f;
+    [SerializeField] private float yRangeMin = -20f;
+
+    [SerializeField] private float smallYRangeMax = 20f;
+    [SerializeField] private float smallYRangeMin = 12f;
+    
     private Vector2 spawnPosition;
+
+    public GameObject misslePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
+
         StartCoroutine(SpawnMissles());
     }
 
@@ -34,41 +51,49 @@ public class MissleSpawnerRP : MonoBehaviour
             yield return new WaitForSeconds(3f);
             spawnPosition = PickRandomSpawn();
             Debug.Log("Missle Spawned at " + spawnPosition);
-           
+
+            Instantiate(misslePrefab, spawnPosition, Quaternion.identity);
         }
     }
 
     private Vector2 PickRandomSpawn()
     {
-        bool addX = Random.Range(0, 2) == 1;
-        bool addY = Random.Range(0, 2) == 1;
+        float randX = Random.Range(xRangeMin, xRangeMax);
+        float randY = Random.Range(yRangeMin, yRangeMax);
+        if (randX < 17.5f && randX > -17.5f)
+        {
+            bool positiveY = Random.Range(0, 2) == 1;
+            if (positiveY)
+            {
+                randY = Random.Range(smallYRangeMin, smallYRangeMax);
+            } 
+            else
+            {
+                randY = Random.Range(-smallYRangeMin, -smallYRangeMax);
+            }
+        }
+        else
+        {
+            randY = Random.Range(yRangeMin, yRangeMax);
+        }
 
-        float randX = Random.Range(minXRange, maxXRange);
-        float randY = Random.Range(minYRange, maxYRange);
-
-        float newXPos = transform.position.x + (addX ? randX : -randX);
-        float newYPos = transform.position.y + (addY ? randY : -randY);
-
-        return new Vector2(newXPos, newYPos);
+        return new Vector2(randX, randY);
     }
 
-
-    [SerializeField] private float gizPositionX = 0f;
-    [SerializeField] private float gizPositionY = 0f;
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(maxXRange, maxYRange, 0));
+        Gizmos.DrawWireCube(transform.position, new Vector3(gizmoMaxXRange, gizmoMaxYRange, 0));
 
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(transform.position, new Vector3(minXRange, minYRange, 0));
+        Gizmos.DrawWireCube(transform.position, new Vector3(gizmoMinXRange, gizmoMinYRange, 0));
 
         Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(spawnPosition, 3f);
+        Gizmos.DrawSphere(spawnPosition, 0.5f);
 
         Gizmos.color = Color.green;
-        Gizmos.DrawSphere(new Vector2(gizPositionX, gizPositionY), 3f);
+        Gizmos.DrawSphere(new Vector2(gizPositionX, gizPositionY), 0.2f);
     }
 
 }
