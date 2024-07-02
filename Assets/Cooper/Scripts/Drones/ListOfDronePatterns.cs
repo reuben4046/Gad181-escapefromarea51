b1ddd72136d2bd436a573b550b35bfc326c70205
Cooper-Rampling <134895307+Cooper-Rampling@users.noreply.github.com>
2024-07-02 17:31:15 +1000

    Added spawn points

---
+++ Assets/Cooper/Scripts/Drones/ListOfDronePatterns.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListOfDronePatterns : MonoBehaviour
+{
+    public List<Drone_Pattern> Patterns = new List<Drone_Pattern>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Cooper/Scripts/Gamemanager.cs
+++ Assets/Cooper/Scripts/Gamemanager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Gamemanager : MonoBehaviour
 {
     bool mouse;
+    public Spawnpoints_List spawnpoints;
+    public GameObject drone;
+    public int maxdrones = 10;
+    private int numberOfdrones;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,11 @@ public class Gamemanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (numberOfdrones < maxdrones)
+        {
+            Instantiate(drone, spawnpoints.spawnPoints[Random.Range(0, spawnpoints.spawnPoints.Count)].transform.position, Quaternion.identity);
+            numberOfdrones += 1;
+        }
     }
 
     
