3ff098d743921b0c2a2a51a0ac37c576e442566a
reuben4046 <poolereuben@gmail.com>
2024-07-29 20:30:27 +1000

    basic logic for new state machine works.
    just need to put the code already created in each separate state and then work out some kind of flow i would like the enemys to follow.

--- Assets/ReubenGame2/Scripts/Enemys/EnemyStates/StateManager/State.cs
+++ Assets/ReubenGame2/Scripts/Enemys/EnemyStates/StateManager/State.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public abstract class State : MonoBehaviour
 {
     public abstract State RunCurrentState();
+    
 }
--- Assets/ReubenGame2/Scripts/Enemys/EnemyStates/StopMovingState.cs
+++ Assets/ReubenGame2/Scripts/Enemys/EnemyStates/StopMovingState.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class StopMovingState : State
 {
     public ShootingState shootingState;
+    //public GoToCoverState goToCoverState;
     public bool canSeePlayer;
 
     public override State RunCurrentState()
@@ -12,12 +15,29 @@ public class StopMovingState : State
         if(canSeePlayer)
         {
             return shootingState;
+            //return goToCoverState;
         }
         else
         {
+            StopMoving();
             return this;
         }
     }
-    
+
+    [SerializeField] NavMeshAgent agentEnemy;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            canSeePlayer = true;
+        }
+    }
+    //stop moving
+    void StopMoving()
+    {
+        agentEnemy.SetDestination(transform.position);
+    }
+
 }
 
